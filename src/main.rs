411786dabec1e1e2f95b1f2853d2a95ef9ff93df
vishal18713 <lcs2023052@iiitl.ac.in>
2024-07-26 12:08:32 +0530

    refactor: Improve guessing game logic and input handling

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
-use std::io;
 use rand::Rng;
+use core::num;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     // Generate a random number between 1 and 100
-    let secret_number = rand::thread_rng().gen_range(1..=10);
-    println!("The secret number is: {secret_number}"); // Debugging purpose, should be commented out or removed in a real game
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // println!("The secret number is: {secret_number}"); // Debugging purpose, should be commented out or removed in a real game
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    // Create a mutable String to store the guess
-    let mut guess = String::new();
+        // Create a mutable String to store the guess
+        let mut guess = String::new();
 
-    // Read the input from the user
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // Read the input from the user
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // Trim the input and print the guess
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        // Trim the input and print the guess
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small"),
-        Ordering::Equal => println!("you win"),
-        Ordering::Greater => println!("too big");  
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            },
+            Ordering::Greater => println!("too big"),
+        }
     }
 }
